6e8f7bbaf52254532dd03df55f79ffba3ea6a50e
Nicole Zamonsky <89232744+nzamonsky@users.noreply.github.com>
2024-06-22 19:34:58 -0300

    Update Materia.java

--- estudiantes/src/main/java/aed/Materia.java
+++ estudiantes/src/main/java/aed/Materia.java
@@ -9,25 +9,25 @@ public class Materia {
     private ParPunteroAlias[] parPunteroArray;
 
     public ParPunteroAlias[] getParPunteroArray() {
-        return parPunteroArray;
+        return parPunteroArray; // O(1)
     }
 
     public void setParPunteroArray(ParPunteroAlias[] parPunteroArray) {
-        this.parPunteroArray = parPunteroArray;
+        this.parPunteroArray = parPunteroArray; // O(1)
     }
 
     public Materia() {
-        this._estudiantes = new DicTrie();
-        this._docentes = new int[] { 0, 0, 0, 0 };
+        this._estudiantes = new DicTrie(); // O(1)
+        this._docentes = new int[] { 0, 0, 0, 0 }; // O(1)
     }
 
 
     public int get_estudiantes() {
-        return _estudiantes._tamaño;
+        return _estudiantes._tamaño; // O(1)
     }
 
     public ArrayList<String> lista_estudiantes() {
-        return new ArrayList<>(_estudiantes.imprimir());
+        return new ArrayList<>(_estudiantes.imprimir()); // O(sum_{c \in C} |c|)
     }
 
     public void agregarEstudiante(String estudiante) {
@@ -37,9 +37,10 @@ public class Materia {
     }
 
     public int[] get_docentes() {
-        return _docentes;
+        return _docentes; // O(1)
     }
 
+    // O(1)
     public void agregar(SistemaSIU.CargoDocente cargo) {
         if (cargo.equals(SistemaSIU.CargoDocente.PROF)) {
             _docentes[0]++;
