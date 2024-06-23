4c25b930d9914debdaa4d0b2b4df453c4100073f
Nicole Zamonsky <89232744+nzamonsky@users.noreply.github.com>
2024-06-23 01:08:33 -0300

    Update InfoMateria.java

--- estudiantes/src/main/java/aed/InfoMateria.java
+++ estudiantes/src/main/java/aed/InfoMateria.java
@@ -1,9 +1,7 @@
 package aed;
 
 public class InfoMateria{
-    // Invariante de Representación
-    // paresCarreraMateria cumple con el invariante de representación de la clase ParCarreraMateria
-
+    
     private ParCarreraMateria[] paresCarreraMateria;
 
     public InfoMateria(ParCarreraMateria[] paresCarreraMateria){
