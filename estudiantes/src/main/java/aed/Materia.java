62b3e2085ac0544aef5829edbcbe1ee924456c67
FRV27 <113072989+FRV27@users.noreply.github.com>
2024-07-13 22:29:35 -0300

    Update Materia.java

--- estudiantes/src/main/java/aed/Materia.java
+++ estudiantes/src/main/java/aed/Materia.java
@@ -29,7 +29,7 @@ public class Materia {
     }
 
     public ArrayList<String> lista_estudiantes() {
-        return new ArrayList<>(_estudiantes.imprimir()); // O(sum_{c \in C} |c|)
+        return new ArrayList<>(_estudiantes.imprimir()); // O(E_m)
     }
 
     public void agregarEstudiante(String estudiante) {
